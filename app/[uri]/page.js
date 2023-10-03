42032ce591bb1b4a6f506937e114880123f8e23a
ManuelNavarro7 <manuel.mnavarro@hotmail.com>
2023-10-03 15:00:51 -0300

    first commit

---
+++ app/[uri]/page.js
@@ -0,0 +1,46 @@
+import { getClient } from "../lib/apollo.js";
+import { gql } from "@apollo/client";
+import Image from "next/image.js";
+export default async function IndPost({ params }) {
+  const queryPostByuri = gql`
+    query PostByUri($uri: String!) {
+      postBy(uri: $uri) {
+        title
+        content
+        featuredImage {
+          node {
+            mediaItemUrl
+          }
+        }
+      }
+    }
+  `;
+
+  const { data } = await getClient().query({
+    query: queryPostByuri, // Use "query" property to specify the query
+    variables: {
+      uri: `https://headlessroadev.wpengine.com/${params.uri}`, // Interpolate the variable correctly
+    },
+    context: {
+      fetchOptions: {
+        next: { revalidate: 5 },
+      },
+    },
+  });
+
+  const post = data?.postBy;
+
+  const imgaF = `${post.featuredImage.node.mediaItemUrl}`;
+
+  return (
+    <main>
+      <div className="siteHeader">
+        <h1 className="title">{post.title}</h1>
+      </div>
+      <div className="div-main-content">
+        <Image alt={post.title} width={700} height={500} src={imgaF}></Image>
+        <article dangerouslySetInnerHTML={{ __html: post.content }}></article>
+      </div>
+    </main>
+  );
+}
